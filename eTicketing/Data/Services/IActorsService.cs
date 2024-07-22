5f8b58225eecbd7aa8e5474bc4d95101f5773852
SelmaSaliaj <134957881+SelmaSaliaj@users.noreply.github.com>
2024-07-22 06:06:02 +0200

    Add files via upload

---
+++ eTicketing/Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using eTicketing.Models;
+
+namespace eTicketing.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+        Task<Actor> GetByIdAsync(int id);
+        Task AddAsync(Actor actor);
+        Task<Actor> UpdateAsync(int id, Actor newActor);
+        Task DeleteAsync(int id);
+    }
+}
